1c969c32fe578045b2906bd3b31fe8137c289bfb
greentangerine <nick@ayle.net>
2020-04-18 16:45:13 +0100

    Changed to use ME3000 class." "

--- endoctopusgo.py
+++ endoctopusgo.py
@@ -2,31 +2,32 @@
 
 import sys
 sys.path.insert(0, '/home/pi/ME3000')
-import ME3000 as me
+from me3000 import ME3000
 from datetime import datetime
 
 SLAVE=0x01
+SERIAL_PORT="/dev/ttyUSB0"
 
-serial_port = me.get_serial_port()
+roo = ME3000(SERIAL_PORT, SLAVE)
 
 print(datetime.now())
 print("Get inverter state ...")
-status, invstate, invstring = me.get_inverter_state(serial_port, SLAVE)
+status, invstate, invstring = roo.get_inverter_state()
 if status:
     print("State = ", invstate, "[", invstring, "]")
 
 print("Get battery percentage ...")
-status, response = me.get_battery_percentage(serial_port, SLAVE)
+status, response = roo.get_battery_percentage()
 if status:
     print(response)
 
 print("Set to auto ...")
 retval = -1
 while retval < 0:
-    status, response = me.set_auto(serial_port, SLAVE)
+    status, response = roo.set_auto()
     if status:
         retval = response & 0x00FF
         if retval != 0:
             print("Set auto failed", hex(response))
 
-me.close_serial_port(serial_port)
+roo.close_serial_port()
--- octopusgo.py
+++ octopusgo.py
@@ -2,11 +2,12 @@
 
 import sys
 sys.path.insert(0, '/home/pi/ME3000')
-import ME3000 as me
+from me3000 import ME3000
 from datetime import datetime
 
 SLAVE=0x01
 THRESHOLD_FILE="/home/pi/ME3000/pct.txt"
+SERIAL_PORT="/dev/ttyUSB0"
 
 print(datetime.now())
 
@@ -19,31 +20,32 @@ except:
     # default to fully charged
     threshold = 100
 
-serial_port = me.get_serial_port()
+roo = ME3000(SERIAL_PORT, SLAVE)
+
 
 print("Charge threshold =", threshold)
 print("Get inverter state ...")
-status, invstate, invstring = me.get_inverter_state(serial_port, SLAVE)
+status, invstate, invstring = roo.get_inverter_state()
 if status:
     print("State = ", invstate, "[", invstring, "]")
 
 print("Get battery percentage ...")
-status, response = me.get_battery_percentage(serial_port, SLAVE)
+status, response = roo.get_battery_percentage()
 if status:
     print(response)
     if response < threshold:
         print("Below threshold, set to charge 3000W ...")
-        status, response = me.set_charge(serial_port, SLAVE, 3000)
+        status, response = roo.set_charge(3000)
         if status:
             retval = response & 0x00FF
             if retval != 0:
                 print("Set charge failed ...")
     elif invstate == 2:
         print("Over threshold and charging, so switch to auto ...")
-        status, response = me.set_auto(serial_port, SLAVE)
+        status, response = roo.set_auto()
         if status:
             retval = response & 0x00FF
             if retval != 0:
                 print("Set auto failed", hex(response))
 
-me.close_serial_port(serial_port)
+roo.close_serial_port()
