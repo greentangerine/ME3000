3f2cd8e9fd09c3b995ba8943b49500cabd5281bd
greentangerine <nick@ayle.net>
2020-04-29 07:39:59 +0100

    update query script

--- query.py
+++ query.py
@@ -1,39 +1,31 @@
-#!/usr/bin/env python
-# scripts/example/simple_rtu_client.py
+#!/usr/bin/env python3
 
-import ME3000 as me
+import sys
+sys.path.insert(0, '/home/pi/ME3000')
 
-SLAVE=0x01
+from me3000 import ME3000
+from MyME3000 import *
 
-serial_port = me.get_serial_port()
+roo = ME3000(SERIAL_PORT, SLAVE)
 
 print("Get all holding registers ...")
-status, response = me.read_holding(serial_port, SLAVE)
+status, response = roo.read_holding()
 if status:
-  print(response)
+    print(response)
 
 print("Get all input registers ...")
-status, response = me.read_input(serial_port, SLAVE)
+status, response = roo.read_input()
 if status:
-  print(response)
+    print(response)
 
 print("Get inverter state ...")
-status, response = me.get_inverter_state(serial_port, SLAVE)
+status, invstate, invstring = roo.get_inverter_state()
 if status:
-  print(hex(response))
+    print("State = ", invstate, "[", invstring, "]")
 
 print("Get battery percentage ...")
-status, response = me.get_battery_percentage(serial_port, SLAVE)
+status, response = roo.get_battery_percentage()
 if status:
   print(response)
 
-#print("Set to discharge 1000W ...")
-#status, response = me.set_discharge(serial_port, SLAVE, 1000)
-#print(hex(response))
-
-print("Set to auto ...")
-status, response = me.set_auto(serial_port, SLAVE)
-if status:
-  print(hex(response))
-
-me.close_serial_port(serial_port)
+roo.disconnect()
